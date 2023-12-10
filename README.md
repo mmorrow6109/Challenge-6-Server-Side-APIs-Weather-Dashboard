e1aa245acb4f352d91564ca85894da5525fbf96b
mmorrow6109 <msmorrow6109@gmail.com>
2023-12-10 15:43:38 -0800

    Functionality, README and CSS updated

---
+++ README.md
@@ -0,0 +1,22 @@
+## Challenge 6: Weather Dashboard
+
+
+## Details
+A functional weather app that uses the OpenWeather API to pull realtime data and display current weather as well as a five-day forecast for the city defined in the search bar.
+
+## Credits
+Matt Morrow
+
+## License
+
+N/A
+
+## Features
+- functional search bar
+- current weather & 5-day forecast
+- saves to local storage
+
+## Link to Live Application
+
+
+## Screenshots
--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ let searchBtnEl = document.querySelector("#search-btn");
 let citySearchTerm = document.querySelector("#city-search-term");
 let weatherContainerEl = document.querySelector(".weather-container");
 let fiveWeatherContainerEl = document.querySelector(".five-weather-container");
+let showingWeather = document.querySelector("#showing-weather");
+let cityHistory = JSON.parse(localStorage.getItem('cityHistory')) || [];
 
 
 let getCity = function (event) {
@@ -16,23 +18,28 @@ let getCity = function (event) {
   .then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
+    
 
-        //the object returned is an array of 40 objects.  We need to pull the specific data we want from the array.
-        let temperatureInKelvin = data.list[5].main.temp; 
-        //we need to convert temp from kelvin to fahrenheit
-        let temperatureInFahrenheit = (temperatureInKelvin - 273.15) * 9/5 + 32; // convert to Fahrenheit
-            temperatureInFahrenheit = temperatureInFahrenheit.toFixed(2); // round down with 2 decimal points
-        let windSpeed = data.list[0].wind.speed; 
-        let humidity = data.list[6].main.humidity; 
+      // clear previous data
+      weatherContainerEl.innerHTML = "";
+      let card = document.querySelector('.five-weather-container');
+      card.innerHTML = "";
 
-        console.log('Temperature: ' + temperatureInFahrenheit + '°F');
-        console.log('Wind Speed: ' + windSpeed);
-        console.log('Humidity: ' + humidity);
+      showingWeather.innerHTML = "";
 
-        weatherContainerEl.innerHTML = "";
+      //the object returned is an array of 40 objects.  We need to pull the specific data we want from the array.
+      let temperature = data.list[5].main.temp;         
+      let windSpeed = data.list[0].wind.speed; 
+      let humidity = data.list[6].main.humidity; 
 
-        displayWeather(data.list[0], weatherContainerEl, city);
-        fiveDayForecast(data);
+      console.log('Temperature: ' + temperature + '°F');
+      console.log('Wind Speed: ' + windSpeed);
+      console.log('Humidity: ' + humidity);
+
+      weatherContainerEl.innerHTML = "";
+
+      displayWeather(data.list[0], weatherContainerEl, city);
+      fiveDayForecast(data, city);
       });
     } else {
       alert('Error: ' + response.statusText);
@@ -40,23 +47,35 @@ let getCity = function (event) {
   })
 
   let searchedCity = document.createElement("button");
-  searchedCity.classList.add("cities");
+  searchedCity.classList.add("cities-btn");
+  searchedCity.id = "city-btn";
   let citySearchTerm = document.createTextNode(city);
   searchedCity.appendChild(citySearchTerm);
 
+  // add event listener
+  searchedCity.addEventListener('click', function() {
+    getCity(this.innerText); // pass the city name to the getCity function?
+  });
+
   let card = document.querySelector('.card-body'); // select the card
   card.appendChild(searchedCity); // append the button to the card
+
+  cityHistory.push(city);
+  localStorage.setItem('cityHistory', JSON.stringify(cityHistory));
 }
 
 let displayWeather = function (list, containerEl, city) {
   console.log(list);
-  let container=document.createElement('div');
-  // clear old content
-  // weatherContainerEl.textContent = "";
-  let currentCity = document.createElement('div');
-  currentCity.textContent = city + " (" + new Date().toLocaleDateString() + ")";
 
-console.log("icon", list);
+  let showingWeather = document.createElement("h3");
+  let display = document.getElementById("showing-weather");
+  // showingWeather.textContent = ('Showing Weather for: ' & city);
+  showingWeather.textContent = (city);
+  display.appendChild(showingWeather);
+
+  let container=document.createElement('p');
+  let currentCity = document.createElement('p');
+  currentCity.textContent = city + " (" + new Date().toLocaleDateString() + ")";
 
   let weatherIcon = document.createElement("img");
   weatherIcon.setAttribute("src", "https://openweathermap.org/img/w/" + list.weather[0].icon + ".png");
